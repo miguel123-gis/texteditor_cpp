595e2adb7f10712b430486df99ddf9a646d33ed1
Miguel Imperial <migimperial@yahoo.com>
2026-07-09 23:33:12 +0800

    WIP Add functions
    - build_app_menu_bar() to create the menu bar
    - set_changed() to create a global flag if filename was changed
    - set_filename()
    - update_title()

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,74 @@
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/fl_ask.H>
+#include <FL/filename.H>
+#include <FL/fl_string_functions.h>
 #include <FL/Fl_Double_Window.H>
-#include <FL/Fl.H>
 
-Fl_Double_Window *app_window = NULL;
+
+namespace Ted {
+    Fl_Double_Window* app_window = NULL;
+    Fl_Menu_Bar* app_menu_bar = NULL;
+    bool text_changed = false;
+    char app_filename[FL_PATH_MAX] = "";
+}
 
 void build_app_window() {
-    app_window = new Fl_Double_Window(640, 480, "FLTK Editor");
+    Ted::app_window = new Fl_Double_Window(640, 480, "FLTK Editor");
 }
+
+void build_app_menu_bar() {
+    Ted::app_window->begin();
+    Ted::app_menu_bar = new Fl_Menu_Bar(0, 0, Ted::app_window->w(), 25);
+    Ted::app_menu_bar->add("File/Quit Editor", FL_COMMAND+'q', menu_quit_callback);
+    Ted::app_window->callback(menu_quit_callback);
+    Ted::app_window->end();
+}
+
+// If content was changed
+void set_changed(bool v) {
+    if (v != Ted::text_changed) {
+        Ted::text_changed = v;
+        update_title();
+    }
+}
+
+// If content was changed
+void set_filename(const char* new_filename) {
+    if (new_filename) {
+        fl_strlcpy(Ted::app_filename, new_filename, FL_PATH_MAX);
+    } else {
+        Ted::app_filename[0] = 0;
+    }
+    update_title();
+}
+
+void update_title() {
+    const char *fname = NULL;
+    if (Ted::app_filename[0]) { // Still zero length
+        fname = fl_filename_name(Ted::app_filename);
+    }
+
+    // If file is already saved
+    if (fname) {
+        char buf[FL_PATH_MAX + 3];      // For the file extension???
+        buf[FL_PATH_MAX + 2] = '\0';    // ensure that the buffer is always null-terminated
+        
+        if (Ted::text_changed) {
+            snprintf(buf, FL_PATH_MAX+2, "%s *", fname);
+        } else {
+            snprintf(buf, FL_PATH_MAX+2, "%s", fname);
+        }
+
+        Ted::app_window->copy_label(buf);
+    } else {
+        Ted::app_window->label("FLTK Editor");
+    }
+}
+
+void menu_quit_callback(Fl_Widget*, void*) { /* TODO */ }
 
 int main(int argc, char **argv) {
     build_app_window();
-    app_window->show(argc, argv);
+    Ted::app_window->show(argc, argv);
     return Fl::run();
 }
