dfec25ff34bcd1f92c07d73a856d586366c383cb
Miguel Imperial <migimperial@yahoo.com>
2026-07-10 23:40:00 +0800

    Add text editor window
    - Add placeholder callback if content is changed

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,27 @@
 #include <FL/filename.H>
 #include <FL/fl_string_functions.h>
 #include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Text_Buffer.H>
+#include <FL/Fl_Text_Editor.H>
 
 
-// Forward declarations
+// FORWARD DECLARATIONS
 void menu_quit_callback(Fl_Widget*, void*);
 void update_title();
+void text_changed_callback();
 
-// Global variables
+// GLOBAL VARIABLES
 namespace Ted {
     Fl_Double_Window* app_window = NULL;
     Fl_Menu_Bar* app_menu_bar = NULL;
+    Fl_Text_Editor* app_editor = NULL;
+    Fl_Text_Editor* app_split_editor = NULL;
+    Fl_Text_Buffer* app_text_buffer = NULL;
     bool text_changed = false;
     char app_filename[FL_PATH_MAX] = "";
 }
 
+// WINDOWS
 void build_app_window() {
     Ted::app_window = new Fl_Double_Window(640, 480, "FLTK Editor");
 }
@@ -29,6 +36,23 @@ void build_app_menu_bar() {
     Ted::app_window->end();
 }
 
+void build_main_editor() {
+    Ted::app_window->begin();
+    Ted::app_text_buffer = new Fl_Text_Buffer();
+    Ted::app_text_buffer->add_modify_callback(text_changed_callback, NULL);
+    Ted::app_editor = new Fl_Text_Editor(
+        0, 
+        Ted::app_menu_bar->h(),
+        Ted::app_window->w(),
+        Ted::app_window->h() - Ted::app_menu_bar->h()
+    );
+    Ted::app_editor->buffer(Ted::app_text_buffer);
+    Ted::app_editor->textfont(FL_COURIER);
+    Ted::app_window->resizable(Ted::app_editor);
+    Ted::app_window->end();
+}
+
+// OTHER FUNCTIONS
 // If content was changed
 void set_changed(bool v) {
     if (v != Ted::text_changed) {
@@ -67,6 +91,12 @@ void update_title() {
         Ted::app_window->copy_label(buf);
     } else {
         Ted::app_window->label("FLTK Editor");
+    }
+}
+
+void text_changed_callback(int, int n_inserted, int n_deleted, int, const char*, void*) {
+    if (n_inserted || n_deleted) {
+        set_changed(true);
     }
 }
 
